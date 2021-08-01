645cf995c83bc9b4499986856b140f0602bf10d2
dcts <thomas@liist.com>
2021-08-01 18:31:32 +0200

    :recycle: refactor index

--- traversy-media-tutorial/src/main.rs
+++ traversy-media-tutorial/src/main.rs
@@ -1,24 +1,26 @@
 // mod testing;
-// mod example_1_print;
-// mod example_2_vars;
-// mod example_3_types;
-// mod example_4_strings;
-// mod example_5_tuples;
-// mod example_6_arrays;
-// mod example_7_vectors;
-// mod example_8_conditionals;
-mod example_9_loops;
+// mod example_01_print;
+// mod example_02_vars;
+// mod example_03_types;
+// mod example_04_strings;
+// mod example_05_tuples;
+// mod example_06_arrays;
+// mod example_07_vectors;
+// mod example_08_conditionals;
+// mod example_09_loops;
+mod example_10_functions;
 
 fn main() {
     // testing::run();
-    // example_1_print::run();
-    // example_2_vars::run();
-    // example_3_types::run();
-    // example_4_strings::run();
-    // example_5_tuples::run();
-    // example_6_arrays::run();
-    // example_7_vectors::run();
-    // example_8_conditionals::run();
-    example_9_loops::run();
+    // example_01_print::run();
+    // example_02_vars::run();
+    // example_03_types::run();
+    // example_04_strings::run();
+    // example_05_tuples::run();
+    // example_06_arrays::run();
+    // example_07_vectors::run();
+    // example_08_conditionals::run();
+    // example_09_loops::run();
+    example_10_functions::run();
 }
 
