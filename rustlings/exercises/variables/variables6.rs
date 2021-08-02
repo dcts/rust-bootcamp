585b2f36b16cb190cef6effa546fdcba17f195cf
dcts <thomas@liist.com>
2021-08-02 18:59:03 +0200

    :tada: init rustlings and do save first session

---
+++ rustlings/exercises/variables/variables6.rs
@@ -0,0 +1,9 @@
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+// @TODO: constant declarations always need type? No type inference for const variables?
+
+const NUMBER: i8 = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
