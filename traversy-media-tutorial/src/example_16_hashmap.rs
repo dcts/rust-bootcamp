1136920f30a01f9c7105ff03ea8f547d3a1916d0
dcts <thomas@liist.com>
2022-05-23 02:26:30 +0200

    :construction: save

---
+++ traversy-media-tutorial/src/example_16_hashmap.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap; // use hashmaps
+
+
+pub fn run() {
+    println!("=== HASH MAPS ===");
+
+    // initialize hashmap
+    let mut birthdayDictionary = HashMap::new();
+
+}
