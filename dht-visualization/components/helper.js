e46878c03a80c6eb362548a680c563eadf63a3bc
dcts <thomas@liist.com>
2022-07-10 16:28:54 +0200

    :construction: WIP

--- dht-visualization/components/helper.js
+++ dht-visualization/components/helper.js
@@ -1,4 +1,3 @@
-
 const hash = input => {
   return crypto.createHash("sha256").update(input).digest("hex");
 }
@@ -11,18 +10,56 @@ const int2hex = num => {
   return num.toString(16);
 }
 
-const computeRanges = n => {
-  const min = hex2int("0000000000000000000000000000000000000000000000000000000000000000");
-  const max = hex2int("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-  const range = max - min;
+const rangesHex = n => {
+  const range = MAX_HASH_INT - MIN_HASH_INT;
   const interval = range / n;
-  const dictInt = new Array(n).fill(1).map((d, indx) => min + indx*interval);
-  const dictHex = dictInt.map(d => int2hex(d));
-  const precision = Math.ceil(n/16);
-  return dictHex.map(d => d.slice(0, precision).padEnd(precision, "0"));
+  const ranges = new Array(n).fill(1).map((_, indx) => MAX_HASH_INT + indx*interval);
+  return ranges.map(d => int2hex(d).padStart(64, "0"));
+}
+
+const rangesInt = n => {
+  return rangesHex(n).map(hex => hex2int(hex));
+}
+
+// CONSTANTS
+const MIN_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+const MAX_HASH = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+const MIN_HASH_INT = hex2int(MIN_HASH);
+const MAX_HASH_INT = hex2int(MAX_HASH);
+
+// const rangeDict = n => {
+//   const ranges = computeRanges(n);
+//   const precision = Math.ceil(n/16);
+//   const dictChar = ranges.map(d => d.slice(0, precision).padEnd(precision, "0"));
+//   const dict = {};
+//   dictChar.forEach((ch, indx) => dict[ch] = indx);
+//   return dict;
+// }
+
+const getNodeIndx = (hash, n) => {
+  return hex2int(hash) / 2;
 }
 
 exports.hash = hash;
 exports.hex2int = hex2int;
 exports.int2hex = int2hex;
-exports.computeRanges = computeRanges;
+exports.rangesHex = rangesHex;
+exports.rangesInt = rangesInt;
+exports.MIN_HASH = MIN_HASH;
+exports.MAX_HASH = MAX_HASH;
+exports.MIN_HASH_INT = MIN_HASH_INT;
+exports.MAX_HASH_INT = MAX_HASH_INT;
+
+/*
+0 START
+1 
+2 
+3 
+4 
+5 
+6 END
+
+1.543 
+
+
+*/
--- dht-visualization/init-dev-env.js
+++ dht-visualization/init-dev-env.js
@@ -2,12 +2,33 @@ const Dht = require("./components/dht");
 const Node = require("./components/helper");
 const Post = require("./components/post");
 const { 
+  MIN_HASH,
+  MAX_HASH,
+  MIN_HASH_INT,
+  MAX_HASH_INT,
   hash, 
   hex2int,
   int2hex,
-  computeRanges,
+  rangesHex,
+  rangesInt,
 } = require("./components/helper");
 
 console.log("\n\n");
 console.log(fs.readFileSync('init-dev-env-message.txt', 'utf8'));
 console.log("\n\n");
+
+const n = 16;
+const min = hex2int("0000000000000000000000000000000000000000000000000000000000000000");
+const max = hex2int("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+const range = max - min;
+const interval = range / n;
+const dictInt = new Array(n).fill(1).map((d, indx) => min + indx*interval);
+const dictHex = dictInt.map(d => int2hex(d).padStart(64, "0"));
+const precision = Math.ceil(n/16);
+
+
+// n = 3;
+// min = 0;
+// max = 6;
+// range = max - min;
+// interval = range / n;
